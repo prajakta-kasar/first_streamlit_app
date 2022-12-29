990e1e676b357e19f7b4634e31f6feb540187ec3
Prajakta Kasar <121612635+prajakta-kasar@users.noreply.github.com>
2022-12-29 17:59:55 +0530

    Update streamlit_app.py
    making changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avacado Toast')
+streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
