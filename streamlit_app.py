56853c3e2e60129957f3e3e42faf714663c1b849
Prajakta Kasar <121612635+prajakta-kasar@users.noreply.github.com>
2022-12-29 18:06:03 +0530

    Update streamlit_app.py
    Adding dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,4 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#my_fruit_list = my_fruit_list.set_index('Fruit')
