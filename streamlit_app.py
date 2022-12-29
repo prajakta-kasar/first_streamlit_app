760c56084741cdb75ca84840a0852c49856026ce
Prajakta Kasar <121612635+prajakta-kasar@users.noreply.github.com>
2022-12-29 18:04:32 +0530

    Update streamlit_app.py
    displaying fruit names

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,5 @@ streamlit.dataframe(my_fruit_list)
 
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
