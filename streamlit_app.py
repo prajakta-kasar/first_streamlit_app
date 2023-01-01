681af04945a5ff7c42506819482a1fed6143903d
Prajakta Kasar <121612635+prajakta-kasar@users.noreply.github.com>
2023-01-01 16:40:26 +0530

    Update streamlit_app.py
    Typing mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
-Streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
